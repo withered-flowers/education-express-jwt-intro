4baac18db68226373275432411c4d5bfa906bc01
Withered Flowers <withered-flowers@users.noreply.github.com>
2024-03-25 09:21:24 +0700

    feat: bump update, comment warning biome

--- sources/a-start/controllers/smartphone-controller.js
+++ sources/a-start/controllers/smartphone-controller.js
@@ -1,69 +1,70 @@
 const { Smartphone } = require("../models/index.js");
 
+// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
 class SmartphoneController {
-  static async getRootSmartphoneHandler(req, res) {
-    try {
-      const smartphones = await Smartphone.findAll({
-        attributes: ["id", "name"],
-      });
+	static async getRootSmartphoneHandler(req, res) {
+		try {
+			const smartphones = await Smartphone.findAll({
+				attributes: ["id", "name"],
+			});
 
-      res.status(200).json({
-        statusCode: 200,
-        data: smartphones,
-      });
-    } catch (err) {
-      res.status(500).json({
-        statusCode: 500,
-        error: err.message,
-      });
-    }
-  }
+			res.status(200).json({
+				statusCode: 200,
+				data: smartphones,
+			});
+		} catch (err) {
+			res.status(500).json({
+				statusCode: 500,
+				error: err.message,
+			});
+		}
+	}
 
-  static async postRootSmartphoneHandler(req, res) {
-    try {
-      const { name, price, qty, UserId } = req.body;
+	static async postRootSmartphoneHandler(req, res) {
+		try {
+			const { name, price, qty, UserId } = req.body;
 
-      const smartphone = await Smartphone.create({
-        name,
-        price,
-        qty,
-        UserId,
-      });
+			const smartphone = await Smartphone.create({
+				name,
+				price,
+				qty,
+				UserId,
+			});
 
-      res.status(201).json({
-        statusCode: 201,
-        data: smartphone,
-      });
-    } catch (err) {
-      res.status(500).json({
-        statusCode: 500,
-        error: err.message,
-      });
-    }
-  }
+			res.status(201).json({
+				statusCode: 201,
+				data: smartphone,
+			});
+		} catch (err) {
+			res.status(500).json({
+				statusCode: 500,
+				error: err.message,
+			});
+		}
+	}
 
-  static async getSmartphoneDetailHandler(req, res) {
-    try {
-      const { smartphoneId } = req.params;
+	static async getSmartphoneDetailHandler(req, res) {
+		try {
+			const { smartphoneId } = req.params;
 
-      const smartphone = await Smartphone.findOne({
-        where: { id: smartphoneId },
-        attributes: {
-          exclude: ["createdAt", "updatedAt"],
-        },
-      });
+			const smartphone = await Smartphone.findOne({
+				where: { id: smartphoneId },
+				attributes: {
+					exclude: ["createdAt", "updatedAt"],
+				},
+			});
 
-      res.status(200).json({
-        statusCode: 200,
-        data: smartphone,
-      });
-    } catch (err) {
-      res.status(500).json({
-        statusCode: 500,
-        error: err.message,
-      });
-    }
-  }
+			res.status(200).json({
+				statusCode: 200,
+				data: smartphone,
+			});
+		} catch (err) {
+			res.status(500).json({
+				statusCode: 500,
+				error: err.message,
+			});
+		}
+	}
 }
 
 module.exports = SmartphoneController;
--- sources/a-start/models/smartphone.js
+++ sources/a-start/models/smartphone.js
@@ -1,26 +1,29 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+// biome-ignore lint/suspicious/noRedundantUseStrict: <explanation>
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Smartphone extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Smartphone.init({
-    name: DataTypes.STRING,
-    price: DataTypes.INTEGER,
-    qty: DataTypes.INTEGER,
-    UserId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Smartphone',
-  });
-  return Smartphone;
+	// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+	class Smartphone extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Smartphone.init(
+		{
+			name: DataTypes.STRING,
+			price: DataTypes.INTEGER,
+			qty: DataTypes.INTEGER,
+			UserId: DataTypes.INTEGER,
+		},
+		{
+			sequelize,
+			modelName: "Smartphone",
+		},
+	);
+	return Smartphone;
 };
