030d5479dd160a12034c185b0350094958b5ac7d
Withered Flowers <withered-flowers@users.noreply.github.com>
2024-03-25 09:29:20 +0700

    fix: typo, add biome comment

--- sources/b-end/migrations/20220102092255-create-user.js
+++ sources/b-end/migrations/20220102092255-create-user.js
@@ -1,30 +1,31 @@
-'use strict';
+// biome-ignore lint/suspicious/noRedundantUseStrict: <explanation>
+"use strict";
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
-  }
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("Users", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				type: Sequelize.STRING,
+			},
+			password: {
+				type: Sequelize.STRING,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("Users");
+	},
 };
